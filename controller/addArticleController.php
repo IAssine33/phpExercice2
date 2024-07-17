81f3a9404535b71c7f43097e970877dbe980e63d
IAssine33 <yassine.zriba@lapiscine.pro>
2024-07-17 17:08:48 +0200

    creation du formulaire

--- controller/addArticleController.php
+++ controller/addArticleController.php
@@ -10,19 +10,32 @@ class addArticleController {
 
     public function addArticle(){
 
-        // controller
-        $title = "tetree";
-        $content = "teretete";
-        $date ="2024-10-10";
 
-        // j'instancie l'ArticleRepository
-        // et j'appelle la méthode insert
-        // on lui donnant les valeurs pour le titre, le contenu et la date
-        // que je veux insérer
-        $articleRepository = new ArticleRepository();
-        
-        // appeler la methode insert, recuperation de la valeur retourner par la methode executer
-        $requestIsOk = $articleRepository->insert($title, $content, $date);
+
+
+        // verifier si la REQUETE est de type POST 
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+            $titre= $_POST['titre'];
+            $content = $_POST['content'];
+            $dateNow = new DateTime("NOW");
+            $date = $dateNow->format('Y-m-d');
+
+                    // j'instancie l'ArticleRepository
+            // et j'appelle la méthode insert
+            // on lui donnant les valeurs pour le titre, le contenu et la date
+            // que je veux insérer
+            $articleRepository = new ArticleRepository();
+            
+            // appeler la methode insert, recuperation de la valeur retourner par la methode executer
+            $requestIsOk = $articleRepository->insert($titre, $content, $date);
+        }    else {
+            $requestIsOk = false;
+
+        }
+
+
+
 
         require_once('../template/page/addArticleView.php');
 
--- template/page/addArticleView.php
+++ template/page/addArticleView.php
@@ -1,17 +1,50 @@
 
-<main>
+<?php 
+require_once('../template/partial/header.php');
+?>
+    <main>
 
-<h1>Ajouter un article</h1>
+    <h1>Ajouter un article</h1>
 
-<!-- Appelez la view depuis le controleur pour l'affichage du message succés ou d'erreur-->
-<?php if ($requestIsOk) { ?>
+    <form method="post">
+        <label for="title">Titre</label>
 
-    <p>L'article a bien été enregistré en BDD</p>
+        <input type="text" name="titre">
+        <label for="content">Contenu</label>
+        <input type="text" name="content">
+        <input type="submit" value="Envoyer le formulaire" />
 
-<?php }else{ ?>
+    </form>
 
-    <p>Erreur, l'article n'a pas été entregistrer</p>
 
-<?php } ?>
 
-</main>
+    <!-- Appelez la view depuis le controleur pour l'affichage du message succés ou d'erreur-->
+
+    
+        
+        <?php if ($requestIsOk) { ?>
+            
+            <p>L'article a bien été enregistré en BDD</p>
+
+    <?php }else{ ?>
+
+        <p>Erreur, l'article n'a pas été entregistrer</p>
+
+    <?php } ?>
+
+    
+
+
+
+
+
+
+
+
+
+
+    </main>
+
+<?php
+require_once('../template/partial/footer.php');
+?>
