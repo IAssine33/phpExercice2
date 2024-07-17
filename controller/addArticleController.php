6851ac8006834f8af4d22145ac8083c23ae403ad
IAssine33 <yassine.zriba@lapiscine.pro>
2024-07-17 14:19:18 +0200

    Modifiez le addArticleController et déplacer tout ce qui concerne la bdd  dans une nouvelle méthode de l'articleRepository

--- controller/addArticleController.php
+++ controller/addArticleController.php
@@ -2,39 +2,29 @@
 <?php
 
 require_once('../config/config.php');
+require_once("../model/ArticleRepository.php");
+
+
+
+
 class addArticleController {
 
     public function addArticle(){
 
-         // Se connecter a la base se donner
-         $dbConnexion = new dbConnection();
-         $pdo = $dbConnexion->connect();
+        // controller
+        $title = "tetree";
+        $content = "teretete";
+        $date ="2024-10-10";
 
-         // Préparer la requête SQL
-        $sql = "INSERT INTO article (titre, content, created_at) VALUES (:titre, :content, :date)";
-        $stmt = $pdo->prepare($sql);
+        // j'instancie l'ArticleRepository
+        // et j'appelle la méthode insert
+        // on lui donnant les valeurs pour le titre, le contenu et la date
+        // que je veux insérer
+        $articleRepository = new ArticleRepository();
+        $articleRepository->insert($title, $content, $date);
 
-        // Définir les paramètres à remplacer avec leurs valeurs
-        $titre = "Exemple de titre";
-        $content = "Exemple de CONTENU";
-        $created_at = "24-04-17";
-
-        //  le bindParam verifie si 
-        $stmt->bindParam(':titre', $titre);
-        $stmt->bindParam(':content', $content);
-        $stmt->bindParam(':date', $created_at);
-
-        // Exécuter la requête, si tout vas bien..,sinon aficher l'erreur..
-
-        if ($stmt->execute()) {
-            echo "Nouveau produit ajouté avec succès";
-        } else {
-            echo "Erreur lors de l'ajout du produit";
-        }
 
     }
-
-
 }
 
 $addArticleController = new addArticleController();
--- model/articleRepository.php
+++ model/articleRepository.php
@@ -4,12 +4,13 @@
 require_once('../config/config.php');
 
 class articleRepository{
+
     // methode de récupération des données
     public function findArticles(){
 
         // se connecter a la base se donner
-        $dbConnexion = new dbConnection();
-        $pdo = $dbConnexion->connect();
+        $dbConnection = new dbConnection();
+        $pdo = $dbConnection->connect();
 
         // recuperation de données (artiles) de la base de donner
         $stmt = $pdo->query("SELECT * FROM article");
@@ -18,4 +19,36 @@ class articleRepository{
         return $articles;
 
     }
+        // la méthode insert permet de sauver des données dans la table article
+        // elle insère le titre, le contenu et la date qu'on lui envoie en parametre
+        public function insert($title, $content, $date){
+
+        // Se connecter a la base se donner
+        $dbConnection = new dbConnection();
+        $pdo = $dbConnection->connect();
+
+        // Préparer la requête SQL
+        $sql = "INSERT INTO article (titre, content, created_at) VALUES (:titre, :content, :date)";
+        $stmt = $pdo->prepare($sql);
+
+        // Définir les paramètres à remplacer avec leurs valeurs
+        $titre = "Exemple de titre";
+        $content = "Exemple de CONTENU";
+        $created_at = "24-04-17";
+
+        //  le bindParam verifie si 
+        $stmt->bindParam(':titre', $titre);
+        $stmt->bindParam(':content', $content);
+        $stmt->bindParam(':date', $created_at);
+
+        // Exécuter la requête, si tout vas bien..,sinon aficher l'erreur..
+
+        if ($stmt->execute()) {
+            echo "Nouveau produit ajouté avec succès";
+        } else {
+            echo "Erreur lors de l'ajout du produit";
+        }
+    }
 }
+
+
